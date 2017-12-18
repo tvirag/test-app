c3143f7547e42742a13b5d51f863c5ab50d3febb
Tamas <tvirag@gmail.com>
2017-12-17 20:15:45 -0700

    up to lecture 21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './alerts/warning/warning.component';
+import { SuccessComponent } from './alerts/success/success.component';
 
 
 @NgModule({
   declarations: [
       AppComponent,
       ServerComponent,
-      ServersComponent
+      ServersComponent,
+      WarningComponent,
+      SuccessComponent
   ],
   imports: [
       BrowserModule,
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-server',
-	templateUrl: './server.component.html'
+	templateUrl: './server.component.html',
+	styleUrls: ['./server.component.css']
 })
+
 export class ServerComponent {
 
 }
